a25b20bd8e2dc5b21309cfff44c5aee74e395d5d
washingtonwei <weibingyang@gmail.com>
2023-02-26 08:08:37 -0600

    This is the first commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# hogwarts-artifacts-online
---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/HogwartsArtifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifactsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactsOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsArtifactsOnlineApplication.class, args);
+    }
+
+}
